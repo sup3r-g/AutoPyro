229baafa1eaa0b19ce68235d6e3c208721c4ed3b
sup3r-g <gleb.a.shevchenko@gmail.com>
2025-02-12 17:52:58 +0300

    Updated Inkscape extension

--- inkscape/custom_attribute.py
+++ inkscape/custom_attribute.py
@@ -19,28 +19,41 @@
 #
 
 import inkex
-from inkex.elements._utils import registerNS
+from inkex.elements._utils import registerNS, NSS, SSN
 
 
 class CustomAttribute(inkex.EffectExtension):
 
     def add_arguments(self, pars):
         pars.add_argument(
+            "-t", "--tab", type=str, default="autopyro", help="Selected Namespace"
+        )
+        pars.add_argument(
             "-ns", "--namespace", type=str, default="autopyro", help="Namespace"
         )
         pars.add_argument("-n", "--name", type=str, default="", help="Name")
         pars.add_argument("-v", "--values", type=str, default="", help="Values")
+        pars.add_argument("-o", "--overwrite", type=bool, default=False, help="Overwrite objects ID")
+    
+    @staticmethod
+    def newNS(namespace, url="http://my_namespace.org/namespace"):
+        NSS[namespace] = url
+        SSN[url] = namespace
 
     def effect(self):
-        registerNS("autopyro", "http://my_namespace.org/namespace")
-
         # Get all the options
-        namespace = self.options.namespace
-        name = self.options.name
+        tab = self.options.tab
+        namespace = str(self.options.namespace).lower().replace(" ", "_")
+        name = str(self.options.name).lower().replace(" ", "_")
         values = self.options.values
 
-        for elem in self.svg.selection.values():
-            elem.set(f"{namespace}:{name}", values)
+        # registerNS(namespace, "http://my_namespace.org/namespace")
+        self.newNS(namespace, "http://my_namespace.org/namespace")
+        for element in self.svg.selection.values():
+            if tab == "label":
+                name, values = element.get("inkscape:label").split(": ")
+
+            element.set(f"{namespace}:{name}", values)
 
 
 if __name__ == "__main__":
